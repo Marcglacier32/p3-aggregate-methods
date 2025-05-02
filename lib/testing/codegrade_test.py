b3186b4d5d193b265f196c4f7d1a3c0840b170a2
Marcglacier32 <mark.thiongo@student.moringaschool.com>
2025-05-02 12:46:29 +0300

    Implement aggregate methods and tests

--- lib/testing/codegrade_test.py
+++ lib/testing/codegrade_test.py
@@ -1,6 +1,29 @@
+import pytest
+from lib.enrollment import Student, Course, Enrollment
 
 class TestCodegrade:
-    '''Codegrade placeholder'''
 
-    def test_codegrade_placeholder(self):
-        assert(True)
+    def test_course_count(self):
+        student = Student("Test Student")
+        course1 = Course("Course 1")
+        course2 = Course("Course 2")
+
+        student.enroll(course1)
+        student.enroll(course2)
+
+        assert student.course_count() == 2
+
+    def test_aggregate_enrollments_per_day(self):
+        # Clear any existing enrollments from previous tests
+        Enrollment.all.clear()
+
+        student1 = Student("Student 1")
+        student2 = Student("Student 2")
+        course = Course("Course A")
+
+        student1.enroll(course)
+        student2.enroll(course)
+
+        enrollment_data = Enrollment.aggregate_enrollments_per_day()
+        assert len(enrollment_data) == 1  # One unique date
+        assert list(enrollment_data.values())[0] == 2  # Two enrollments
